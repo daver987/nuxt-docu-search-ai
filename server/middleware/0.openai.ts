6da3202e60a5461d0d8243e94f49ad15c8360b66
daver987 <daveyalexander@outlook.com>
2023-07-05 07:14:27 -0400

    Fix: Move runtime config inside the event handler when injecting the api key into the context

--- server/middleware/0.openai.ts
+++ server/middleware/0.openai.ts
@@ -1,22 +1,21 @@
 import { Configuration, OpenAIApi } from 'openai'
 
-const configuration = new Configuration({
-  apiKey: useRuntimeConfig().OPENAI_API_KEY,
-})
-let openAIConfig = new OpenAIApi(configuration)
-
-type OpenAI = typeof openAIConfig
-
-let openai: OpenAI
-
-declare module 'h3' {
-  interface H3EventContext {
-    openai: OpenAI
-  }
-}
-openai = new OpenAIApi(configuration)
-
 export default eventHandler((event) => {
+  const configuration = new Configuration({
+    apiKey: useRuntimeConfig().OPENAI_API_KEY,
+  })
+  let openAIConfig = new OpenAIApi(configuration)
+
+  type OpenAI = typeof openAIConfig
+
+  let openai: OpenAI
+
+  declare module 'h3' {
+    interface H3EventContext {
+      openai: OpenAI
+    }
+  }
+  openai = new OpenAIApi(configuration)
   if (!openai) {
     openai = new OpenAIApi(configuration)
   }
